d212dc501e0f66222e1a08547906fd116d9ff485
samuzaffar99 <sam.muzaffar99@gmail.com>
2021-10-10 23:43:53 +0500

    add firebase and whatsapp integration, support for !list and !search

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, Response, Request, HTTPException
+from twilio.twiml.messaging_response import MessagingResponse
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 # from twilio.twiml.messaging_response import MessagingResponse
 
 # resp = MessagingResponse()
@@ -6,11 +10,22 @@ from fastapi import FastAPI, Request
 # msg.body('this is the response text')
 # msg.media('https://example.com/path/image.jpg')
 
-app = FastAPI()
+
+project_id = "herwork-tbs"
+# Use a service account
+cred = credentials.Certificate(r"C:\Users\cools\Downloads\herwork-service.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+users_ref = db.collection(u'users')
+services_ref = db.collection(u'services')
+# cd c:/Users/cools/Desktop/GitHub/her_work_server/
 # to run server
+# python -m uvicorn main:app --reload
 # uvicorn main:app --reload
 # to access it from the internet
 # ngrok http 8000
+app = FastAPI()
 
 # Get method to test whether server is working
 @app.get("/")
@@ -21,14 +36,37 @@ def read_root():
 
 # Twilio Calls this post method
 @app.post("/")
-async def print_root(request: Request):
+async def print_root(request: Request, From: str = Form(...), Body: str = Form(...) ):
     print("called post/")
-    print(request)
-    print(request.headers)
-    # The data and event_handler name
-    data = await request.json()
-    print(data)
+    print(Body)
+    print(type(From))
+    if(Body=="!list"):
+        docs = services_ref.where(u'contactNum', u'==', '+923172109965').stream()
+        responseStr : str = f"Your Services:\n"
+        for doc in docs:
+            map = doc.to_dict()
+            # print(f'{doc.id} => {map}')
+            responseStr += map["serviceName"]+ "\n"
+            for offer in map["offers"]:
+                responseStr += offer["offerName"]+ "\n"
+            responseStr += "\n"
+        response = MessagingResponse()
+        msg = response.message(responseStr)
+        return Response(content=str(response), media_type="application/xml")
+    elif(Body=="!search"):
+        docs = services_ref.stream()
+        responseStr : str = f"Your Results:\n"
+        for doc in docs:
+            map = doc.to_dict()
+            if("Chef" in map["serviceName"]):
+                # print(map)
+                responseStr += map["serviceName"]+ " " + map["contactNum"]+"\n"
+        print(responseStr)
+        response = MessagingResponse()
+        msg = response.message(responseStr)
+        return Response(content=str(response), media_type="application/xml")
+    # print(request.headers)
     # Request Source ip
-    client_host = request.client.host
-    print(client_host)
+    # client_host = request.client.host
+    # print(client_host)
     return {"Hello": "World"}
